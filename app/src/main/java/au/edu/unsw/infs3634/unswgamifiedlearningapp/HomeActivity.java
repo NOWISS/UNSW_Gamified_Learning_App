2b2ef3222116d7acaf70ef82b0b8a75cf604bd66
Dilusha2 <dilhima@hotmail.com>
2021-10-23 00:23:02 +1100

    Draft 2

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/HomeActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/HomeActivity.java
@@ -2,13 +2,27 @@ package au.edu.unsw.infs3634.unswgamifiedlearningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button btnLearn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().setTitle("Home");
+
+        btnLearn = (Button) findViewById(R.id.btnLearn1);
+        btnLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, LearnActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/LearnActivity.java
@@ -0,0 +1,15 @@
+package au.edu.unsw.infs3634.unswgamifiedlearningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+        getSupportActionBar().setTitle("Learn");
+    }
+}
